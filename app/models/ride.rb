e9896ee55e55c6f87b81c9418e417af7e5103809
Dewey <hammersdrichard@gmail.com>
2021-05-14 07:06:59 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,7 @@
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  belongs_to :user
+
+  validates :attraction_id, presence: true
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :nausea, presence: true
+  validates :happiness, presence: true
+  validates :tickets, presence: true
+  validates :height, presence: true
 end
