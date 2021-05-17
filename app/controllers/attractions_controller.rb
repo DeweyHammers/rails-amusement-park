0a73c19f6ec3e2a9cd484901fab7469ec58ea67f
Dewey <hammersdrichard@gmail.com>
2021-05-17 13:29:30 -0700

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,4 +2,8 @@ class AttractionsController < ApplicationController
   def index 
     @attractions = Attraction.all
   end
+
+  def show
+    @attractions = Attraction.find_by(id: params[:id])
+  end
 end
